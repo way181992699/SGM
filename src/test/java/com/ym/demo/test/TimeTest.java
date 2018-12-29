58055a1b28a0ad19be671907509f292fd4602b31
王凯星 <1273870341@163.com>
2018-12-29 17:41:42 +0800

    conmit something test

--- src/test/java/com/ym/demo/test/TimeTest.java
+++ src/test/java/com/ym/demo/test/TimeTest.java
@@ -2,6 +2,7 @@ package com.ym.demo.test;
 
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -18,10 +19,36 @@ public class TimeTest {
     public void simlpe1() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Long a = new Long(1544406582);
-        Date date = new Date(a*1000);
+        Date date = new Date(a * 1000);
         System.out.println(date.getTime());
         String format = simpleDateFormat.format(date);
         System.out.println(format);
     }
+
+    @Test
+    public void DateFormatTest() throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Date parse = simpleDateFormat.parse("2018-12-29 09:30:40");
+        System.out.println(parse);
+        System.out.println(parse.getTime());
+    }
+
+    @Test //两个时间的前后时间比较
+    public void DateAfterAndBefor() throws ParseException {
+
+        Long a = Long.valueOf(1546071184);
+        Long b = Long.valueOf(1546071449);
+
+        Date dateA = new Date(a*1000);//16:13:04 CST 2018
+        Date dateB = new Date(b*1000);//16:17:29 CST 2018
+        System.out.println(dateA);
+        System.out.println(dateB);
+        System.out.println(dateA.after(dateB));
+        System.out.println(dateA.before(dateB));
+        System.out.println(dateB.before(dateA));
+        System.out.println(dateB.after(dateA));
+    }
+
 
 }
