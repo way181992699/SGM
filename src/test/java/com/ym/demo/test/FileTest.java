87aeee9ae1b4c5f7bed4b9f3eca3d756b947897e
wkx <1273870341@163.com>
2020-09-14 14:17:21 +0800

    1.http获取txt文件

--- src/test/java/com/ym/demo/test/FileTest.java
+++ src/test/java/com/ym/demo/test/FileTest.java
@@ -14,7 +14,9 @@ import jcifs.smb.SmbFile;
 import org.junit.Test;
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.*;
 
 public class FileTest {
@@ -69,6 +71,35 @@ public class FileTest {
 
         }
 //        System.out.println(lst);
+    }
+
+
+    @Test//测试读取txt文件上的json格式数据
+    public void fun111() throws IOException {
+        String res = null;
+        try {
+            URL url = new URL("http://10.153.105.30/Data/BitDataStatistics/REAL.txt");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            //设置超时间为3秒
+            conn.setConnectTimeout(3 * 1000);
+            //防止屏蔽程序抓取而返回403错误
+            conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+            //得到输入流
+
+            InputStream inputStream = conn.getInputStream();
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            while ((len = inputStream.read(buffer)) != -1) {
+                bos.write(buffer, 0, len);
+            }
+            bos.close();
+           res= new String(bos.toByteArray(), "utf-8");
+
+        } catch (Exception e) {
+            System.out.println("通过url地址获取文本内容失败");
+        }
+        System.out.println(res);
     }
 
     /**
